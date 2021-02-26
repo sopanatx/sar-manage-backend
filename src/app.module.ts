12129b17ba681b0ee2f560cd2064ce6fb44c1ef7
Sopanat Suradach <sopanus2014@gmail.com>
2021-02-27 03:48:56 +0700

    restore prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app/app.service';
 import { AppResolver } from './app/app.resolver';
+import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -16,18 +16,9 @@ import { AuthModule } from './auth/auth.module';
       installSubscriptionHandlers: true,
       context: ({ req, res, connection }) => ({ req, res, connection }),
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 15060,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [],
-      synchronize: true,
-    }),
+
     AuthModule,
   ],
-  providers: [AppService, AppResolver],
+  providers: [AppService, AppResolver, PrismaService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  imports: [],
-  providers: [AuthService, AuthResolver],
+  imports: [PrismaService],
+  providers: [AuthService, AuthResolver, PrismaService],
 })
 export class AuthModule {}
