10df86cd26f47cb7adb3cbff2a05b2fc1b6e47ac
Sopanat Suradach <sopanus2014@gmail.com>
2021-02-27 03:04:44 +0700

    feat : Fix SSL Listen on Production.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as helmet from 'helmet';
+import * as fs from 'fs';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.use(
     helmet({
       contentSecurityPolicy: false,
@@ -12,6 +18,37 @@ async function bootstrap() {
   app.use(helmet.xssFilter());
   app.use(helmet.hidePoweredBy());
   app.enableCors();
-  await app.listen(7001);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 7001);
+  Logger.log('Application Listening on HTTP');
 }
-bootstrap();
+async function bootstraps() {
+  let httpsOptions = null;
+  if (process.env.SSL) {
+    httpsOptions = {
+      key: fs.readFileSync(process.env.SSL_KEY),
+      cert: fs.readFileSync(process.env.SSL_CERT),
+    };
+  }
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+    httpsOptions,
+  });
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      referrerPolicy: { policy: 'no-referrer' },
+    }),
+  );
+  app.use(helmet.xssFilter());
+  app.use(helmet.hidePoweredBy());
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 7001);
+  Logger.log('Application Listening on HTTPS');
+}
+if (process.env.SSL == 'true') {
+  bootstraps();
+} else {
+  bootstrap();
+}
