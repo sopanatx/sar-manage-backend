5c43dafcf5f0ad67f8d4ce22ef9ea11de975c202
Sopanat Suradach <sopanus2014@gmail.com>
2021-05-28 11:26:01 +0700

    Update documents.service.ts

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -5,7 +5,7 @@ import {
 import { BadRequestException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { GraphQLUpload } from 'apollo-server-express';
-import { createWriteStream, stat } from 'fs';
+import { createWriteStream, stat, fs } from 'fs';
 import * as path from 'path';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { minioClient } from '../service/minioClient';
@@ -45,7 +45,7 @@ export class DocumentsService {
     //if not then throw error to user about wrong file
     //TODO: Change filename and upload to s3
     filename = user.username + '_' + Date.now() + `_0` + ext;
-    const fileStream = await createReadStream(filename);
+    const fileStream = await fs.createReadStream(filename);
 
     try {
       await minioClient.putObject(
